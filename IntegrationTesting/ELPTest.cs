7baa5d2465a55d7b40be27c571b0295b2273be51
ESD <ESD@DESKTOP-SNRCVRJ>
2020-12-27 21:53:03 +0600

    Add project files.

---
+++ IntegrationTesting/ELPTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class ELPTest
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TestCase()
+        {
+            driver.Navigate().GoToUrl("http://elp.duetbd.org/");
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("loginbtn")).Click(); //as the web browser has my credentials saved
+            driver.FindElement(By.XPath("//div[@id='page-container-2']/div/div/div/a/div")).Click();
+            driver.FindElement(By.Id("action-menu-toggle-1")).Click();
+            driver.FindElement(By.Id("actionmenuaction-6")).Click();
+            driver.FindElement(By.LinkText("Log in")).Click();
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Click();
+            // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | id=username | ]]
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
